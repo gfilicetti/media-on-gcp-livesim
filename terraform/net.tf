dced8f2d768f684144c32785672cd88348f885ad
Gino Filicetti <ginof@google.com>
2025-08-30 08:42:14 -0400

    First pass of adding our tf work from media-on-gcp This should create a 5 instance MIG

---
+++ terraform/net.tf
@@ -0,0 +1,130 @@
+# Default VPC
+module "vpc" {
+  source  = "terraform-google-modules/network/google//modules/vpc"
+  version = "~> 10.0.0"
+
+  project_id              = local.project.id
+  network_name            = "vpc-livesim"
+  routing_mode            = "GLOBAL"
+  auto_create_subnetworks = true
+}
+
+# Load balancer
+# MIGHT BE NEEDED - Keep for now
+# module "gclb" {
+#   source = "./infra/gclb/stable"
+
+#   project_id = local.project.id
+#   region     = var.gcp_region
+
+#   network = module.vpc.network_self_link
+
+#   backend_services = {
+#     nea = {
+#       instance_group       = module.ateme.nea_instance_group_manager_instance_group
+#       port                 = module.ateme.nea_named_ports[0].port # 8080
+#       port_name            = module.ateme.nea_named_ports[0].name # http1
+#       protocol             = "HTTP"
+#       healthcheck_protocol = "tcp"
+#       enable_cdn           = false
+#     }
+#     # TODO: CDN is nea on port 80
+#     cdn = {
+#       instance_group       = module.ateme.nea_instance_group_manager_instance_group
+#       port                 = module.ateme.nea_named_ports[1].port # 80
+#       port_name            = module.ateme.nea_named_ports[1].name # http2
+#       protocol             = "HTTP"
+#       healthcheck_protocol = "tcp"
+#       enable_cdn           = true
+#     }
+#     titan = {
+#       instance_group       = module.ateme.titan_instance_group_manager_instance_group
+#       port                 = module.ateme.titan_named_ports[0].port # 443
+#       port_name            = module.ateme.titan_named_ports[0].name # https
+#       protocol             = "HTTPS"
+#       healthcheck_protocol = "tcp"
+#       enable_cdn           = false
+#     }
+#     darwin = {
+#       instance_group       = module.darwin.darwin_instance_group_manager_instance_group
+#       port                 = module.darwin.darwin_named_ports[0].port # 443
+#       port_name            = module.darwin.darwin_named_ports[0].name # https
+#       protocol             = "HTTPS"
+#       healthcheck_protocol = "tcp"
+#       enable_cdn           = false
+#     }
+#     "gemini" = {
+#       instance_group       = module.norsk_ai.instance_group_manager_instance_group
+#       port                 = module.norsk_ai.named_ports[0].port # 443
+#       port_name            = module.norsk_ai.named_ports[0].name # https
+#       protocol             = "HTTPS"
+#       healthcheck_protocol = "tcp"
+#       enable_cdn           = false
+#     }
+#     "norsk" = {
+#       instance_group       = module.norsk_gw.instance_group_manager_instance_group
+#       port                 = module.norsk_gw.named_ports[0].port # 443
+#       port_name            = module.norsk_gw.named_ports[0].name # https
+#       protocol             = "HTTPS"
+#       healthcheck_protocol = "tcp"
+#       enable_cdn           = false
+#     }
+#   }
+# }
+
+# General Firewall Rules
+
+# Punch a hole for internal VM to VM traffic
+resource "google_compute_firewall" "fwr_allow_internal" {
+  name          = "fwr-ingress-allow-internal"
+  network       = module.vpc.network_self_link
+  source_ranges = ["10.128.0.0/9"]
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+  allow {
+    protocol = "icmp"
+  }
+}
+
+# Punch a hole for IAP traffic
+resource "google_compute_firewall" "fwr_allow_iap" {
+  name          = "fwr-ingress-allow-iap"
+  network       = module.vpc.network_self_link
+  source_ranges = ["35.235.240.0/20"]
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
+
+resource "google_compute_firewall" "fwr_ssh" {
+  name    = "fwr-ingress-allow-ssh"
+  network = module.vpc.network_self_link
+
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "fwr_http" {
+  name    = "fwr-ingress-allow-http"
+  network = module.vpc.network_self_link
+
+  allow {
+    ports    = ["80", "443"]
+    protocol = "tcp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+### NOTE: Norsk specific firewall rules are defined in norsk module
