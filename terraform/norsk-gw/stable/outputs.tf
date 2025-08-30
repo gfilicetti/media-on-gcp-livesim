894c4a8a0afa13762fbd023c4693590753db6c6b
Gino Filicetti <ginof@google.com>
2025-08-30 11:59:32 -0400

    changing the inner module name to "compute" as per Gemini

--- terraform/norsk-gw/stable/outputs.tf
+++ terraform/norsk-gw/stable/outputs.tf
@@ -1,26 +1,26 @@
 output "network_interfaces" {
   description = "The list of network interfaces configured for the Norsk Gateway instances, passed through from the compute module."
-  value       = module.norsk_gw.network_interfaces
+  value       = module.compute.network_interfaces
 }
 
 output "instance_group_manager_instance_group" {
   description = "The full URL of the Norsk GW instance group created by the manager"
-  value = module.norsk_gw.instance_group_manager_instance_group
+  value       = module.compute.instance_group_manager_instance_group
 }
 
 output "instance_group_manager_self_link" {
   description = "The self-link of the Norsk GW instance group manager."
-  value       = module.norsk_gw.instance_group_manager_self_link
+  value       = module.compute.instance_group_manager_self_link
 }
 
 output "instance_template_self_link" {
   description = "The self-link of the Norsk GW instance template."
-  value       = module.norsk_gw.instance_template_self_link
+  value       = module.compute.instance_template_self_link
 }
 
 output "named_ports" {
   description = "A list of named port configurations for the instance group."
-  value       = module.norsk_gw.named_ports
+  value       = module.compute.named_ports
 }
 
 output "admin_user" {
@@ -34,12 +34,12 @@ output "admin_password" {
   sensitive   = true
 }
 
-# output "port" {
-#   description = "The port for the Norsk GW instance group."
-#   value       = module.norsk_gw.port
-# }
+output "port" {
+  description = "The port for the Norsk GW instance group."
+  value       = module.compute.port
+}
 
-# output "port_name" {
-#   description = "The port name for the Norsk GW instance group."
-#   value       = module.norsk_gw.port_name
-# }
+output "port_name" {
+  description = "The port name for the Norsk GW instance group."
+  value       = module.compute.port_name
+}
