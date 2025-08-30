d07ca625374cedd895b10faca2881c30cb77a1e2
Gino Filicetti <ginof@google.com>
2025-08-30 11:19:11 -0400

    Making sure we expose network interfaces

--- terraform/compute/stable/outputs.tf
+++ terraform/compute/stable/outputs.tf
@@ -1,3 +1,8 @@
+output "network_interfaces" {
+  description = "The list of network interfaces configured for the instances."
+  value       = local.network_interfaces
+}
+
 output "instance_group_manager_instance_group" {
   description = "The full URL of the instance group created by the manager"
   value       = google_compute_region_instance_group_manager.mig.instance_group
@@ -27,7 +32,6 @@ output "port_name" {
   description = "The first port name from the list of named ports."
   value       = length(var.named_ports) > 0 ? var.named_ports : null
 }
-
 
 # output "port" {
 #   description = "The first port from the list of named ports."
